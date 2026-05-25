7d3ff28893b3702cc3476a995c2774624cc7c23d
Luciano-A-Vilete <lvilete1@byupathway.edu>
2026-05-24 22:36:44 -0300

    chapter 12 adjustments part 2

--- nextjs-dashboard/app/lib/actions.tsx
+++ nextjs-dashboard/app/lib/actions.tsx
@@ -3,7 +3,7 @@
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import postgres from 'postgres';
+import postgres, { State } from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -17,7 +17,7 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
